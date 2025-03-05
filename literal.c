4df68bb1bc28473c2f30f3ce1d5e291d8857af8f
bubblerion <windowrains@naver.com>
2025-03-05 02:39:48 +0000

    누락처리

--- literal.c
+++ literal.c
@@ -3,7 +3,6 @@ int main(){
     const int a;
     const int b;
     
- 
     printf("%d %d %d\n" , 100, 0144, 0x64);
     printf("%d 0%o 0x%x\n" , 100, 0144, 0x64);
 
@@ -14,6 +13,6 @@ int main(){
     printf("%c %c %c\n" , 65 , 97 , 48);
     printf("%c %c %c\n" , 'A' , 'A'+1 , 'A'+2);
 
-    printf("%c %c %c\n" , 65 , 97 , 48);
+    printf("%s\n" ,"ABCD");
     return 1;
 }
