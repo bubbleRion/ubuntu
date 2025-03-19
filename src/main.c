7b474defc47a0f928d97e74dc9ef2242c6dbb8c1
bubbelRion <windowrains@naver.com>
2025-03-19 01:35:17 +0000

    main upload

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
-#include "lcd.h"
 #include <avr/io.h>
 #include <avr/delay.h>
-#include <stdio.h>
+
 int main()
 {
+    DDRC = 0x01; // port 0번 비트 출력
+    PORTE = 0xFF; // 풀업 설정
+    PORTC = 0x00;
+    while (1)
+    {
+        _delay_ms(20);
+        if(!(PINE >> 2& _BV(PC0))){
+            PORTC ^= 0x01; // 버튼 반전
+            //버튼이 눌릴 때 까지 대기
+            while(!(PINE >> 2) & _BV(PC0)){
+                _delay_ms(20);
+            }
+        }
+    }
+    
+    
     return 0;
 }
